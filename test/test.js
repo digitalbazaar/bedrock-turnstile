fdb2366c5721b7d0a491b3b1c1a492b236eca4f0
David I. Lehn <dlehn@digitalbazaar.com>
2023-10-11 22:47:24 -0400

    Add initial files.

---
+++ test/test.js
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
+ */
+import * as bedrock from '@bedrock/core';
+import '@bedrock/turnstile';
+import '@bedrock/test';
+
+bedrock.start();
